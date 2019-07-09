5387d9bde61358f1c94112b6a71caeabed66b986
KyleK <kylekatarnls@gmail.com>
2019-07-09 08:03:43 +0200

    Add CLI bootstrap

---
+++ src/Carbon/Cli.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Carbon;
+
+use Carbon\Command\Macro;
+use SimpleCli\SimpleCli;
+
+class Cli extends SimpleCli
+{
+    protected $name = 'carbon';
+
+    public function getCommands(): array
+    {
+        return [
+            'macro' => Macro::class,
+        ];
+    }
+}
---
+++ src/Carbon/Command/Macro.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Carbon\Command;
+
+use Carbon\Cli;
+use SimpleCli\Command;
+use SimpleCli\SimpleCli;
+
+class Macro implements Command
+{
+    public function run(SimpleCli $cli): bool
+    {
+        /* @var Cli $cli */
+    }
+}
